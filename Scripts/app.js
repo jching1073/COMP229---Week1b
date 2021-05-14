15c944bf38eac30cb1fb84dc282d290af98b46d0
Jerome Ching <nathan_9_9@hotmail.com>
2021-05-14 17:56:35 -0400

    Initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,12 @@
+"use strict";
+(function()
+{
+    function Start()
+    {
+        let myVariable = 10;
+        console.log ("My Number is: " + myVariable);
+    }
+    
+    window.addEventListener("load", Start);
+})();
+
